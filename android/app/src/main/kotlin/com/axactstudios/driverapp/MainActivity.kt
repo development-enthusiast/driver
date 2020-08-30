4ffac38f1fe3e61e42a1d83eb36b3744adbf7d6b
Chitransh Jain <cjsmcs2810@gmail.com>
2020-08-30 21:15:49 +0530

    Initial commit

---
+++ android/app/src/main/kotlin/com/axactstudios/driverapp/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.axactstudios.driverapp
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/Screens/SplashScreen.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+class SplashScreen extends StatefulWidget {
+  @override
+  _SplashScreenState createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    new Future.delayed(Duration(seconds: 3), () {});
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final pHeight = MediaQuery.of(context).size.height;
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: Center(
+        child: Image.asset(
+          'images/taxi.png',
+          height: pHeight * 0.35,
+        ),
+      ),
+    );
+  }
+}
